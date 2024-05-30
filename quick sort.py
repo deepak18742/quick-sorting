e29e3ea572397bdf67e2cf84ebec94278dd15389
Deepak <deepak18742@sscbs.du.ac.in>
2024-05-30 13:44:01 +0530

    this is quick soring algo

---
+++ quick sort.py
@@ -0,0 +1,17 @@
+def quicksort(arr):
+
+    if len(arr) <= 1:
+
+     return arr
+
+    else:
+
+        pivot = arr[0]
+
+        left = [x for x in arr[1:] if x < pivot]
+
+        right = [x for x in arr[1:] if x >= pivot]
+
+        return quicksort(left) + [pivot] + quicksort(right)
+
+arr = [5, 1, 8, 3, 2]
